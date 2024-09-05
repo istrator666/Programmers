0db5b9bc1e96da6dff4818607d29a4caf284258f
Tabibito <cyh5343@naver.com>
2024-09-05 18:55:06 +0900

    [level 0] Title: 문자열 출력하기, Time: 1.56 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+    string str;
+    cin >> str;
+    if (1 <= str.length() <= 1000000)
+    {
+        cout<<str;
+    }
+
+    return 0;
+}
