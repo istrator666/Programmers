b3a33370ef68cdfc39e67e7b5e18a5710c0ccd9e
Tabibito <cyh5343@naver.com>
2024-09-05 21:00:25 +0900

    [level 0] Title: 대문자로 바꾸기, Time: 0.01 ms, Memory: 3.63 MB -BaekjoonHub

---
+++ 프로그래머스/0/181877. 대문자로 바꾸기/대문자로 바꾸기.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+string solution(string myString) {
+    string answer = "";
+    
+    transform(myString.begin(), myString.end(), myString.begin(), ::toupper);
+    
+    answer = myString;
+    
+    return answer;
+}
