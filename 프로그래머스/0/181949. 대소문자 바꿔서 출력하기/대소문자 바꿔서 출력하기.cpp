c43ff7a3e00f1a5b26073a4dff2470ac044c724a
Tabibito <cyh5343@naver.com>
2024-09-05 18:57:05 +0900

    [level 0] Title: 대소문자 바꿔서 출력하기, Time: 1.69 ms, Memory: 4.03 MB -BaekjoonHub

---
+++ 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+    string str;
+    cin >> str;
+    
+    for(int i = 0; i < str.length(); i++)
+    {        
+        if (str[i] < 'a')
+        {
+            str[i] += 32;
+        }
+        else
+        {
+            str[i] -= 32;
+        }
+    }
+    
+    cout << str;
+        
+    return 0;
+}
