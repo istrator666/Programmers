52a4151f9a8c73593410baa12fcc19649c8fa1f1
Tabibito <cyh5343@naver.com>
2024-09-05 20:56:35 +0900

    [level 0] Title: n 번째 원소까지, Time: 0.02 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/0/181889. n 번째 원소까지/n 번째 원소까지.cpp
@@ -0,0 +1,10 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> num_list, int n) {
+   vector<int> answer;
+    
+    return answer = vector<int>(num_list.begin(), num_list.begin() + n);
+}
