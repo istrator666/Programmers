cc6df2d129539283c84dd76d9306a1f4b5799a3e
Tabibito <cyh5343@naver.com>
2024-09-05 20:56:45 +0900

    [level 0] Title: n개 간격의 원소들, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/0/181888. n개 간격의 원소들/n개 간격의 원소들.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> num_list, int n) {
+    vector<int> answer;
+    
+    for (int i = 0; i < num_list.size(); i++)
+    {
+        answer.push_back(num_list[i]);
+        i += n -1;
+    }
+    
+    return answer;
+    
+}
