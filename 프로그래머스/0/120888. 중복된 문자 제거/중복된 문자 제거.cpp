d5b56409a6b088b5bf651e32b491b8447e738b10
Tabibito <cyh5343@naver.com>
2024-09-20 20:10:46 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+string solution(string my_string) {
+    string answer = "";
+    
+    for (int i = 0; i < my_string.size(); i++)
+    {
+        if (answer.find(my_string[i]) == string::npos)
+        {
+            answer += my_string[i];
+        }
+    }
+    
+    
+    
+    return answer;
+}
