6cc30e509febede937939c07d2a8aaec6e4eabbc
Tabibito <cyh5343@naver.com>
2024-09-20 22:44:37 +0900

    [level 0] Title: 이진수 더하기, Time: 0.01 ms, Memory: 3.63 MB -BaekjoonHub

---
+++ 프로그래머스/0/120885. 이진수 더하기/이진수 더하기.cpp
@@ -0,0 +1,29 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+string solution(string bin1, string bin2) {
+    string answer = "";
+    
+    int ibin1 = stoi(bin1, nullptr, 2);
+    int ibin2 = stoi(bin2, nullptr, 2);
+    int sum = ibin1 + ibin2;
+    
+if (sum == 0) 
+    {
+        return "0";
+    }
+    
+    
+    while (sum > 0) 
+    {
+        answer += (sum % 2 == 0) ? "0" : "1";
+        sum /= 2;
+    }
+
+    reverse(answer.begin(), answer.end());
+    
+    return answer;
+}
