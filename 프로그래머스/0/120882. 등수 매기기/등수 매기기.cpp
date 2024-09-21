e3fdd7d41f2d28ac22ef056a4ade94bae81b1a17
Tabibito <cyh5343@naver.com>
2024-09-21 23:48:51 +0900

    [level 0] Title: 등수 매기기, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/0/120882. 등수 매기기/등수 매기기.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<vector<int>> score) {
+    vector<int> answer;
+    vector<double> avg;
+    
+    for (int i = 0; i < score.size(); i++)
+    {
+        avg.push_back((score[i][0] + score[i][1]) / 2.0);
+    }
+    
+    for (int i = 0; i < avg.size(); i++)
+    {
+        int rank = 1;
+        for (int j = 0; j < avg.size(); j++)
+        {
+            if (avg[i] < avg[j])
+            {
+                ++rank;
+            }
+        }
+        
+        answer.push_back(rank);
+    }
+    
+    
+    return answer;
+}
