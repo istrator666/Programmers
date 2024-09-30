21033128cd347da18b796ac0bd6c73e3a3dcb7d8
Tabibito <cyh5343@naver.com>
2024-09-30 16:02:45 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.01 ms, Memory: 3.67 MB -BaekjoonHub

---
+++ 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> emergency) {
+    vector<int> answer;
+    
+    for (int i = 0; i < emergency.size(); ++i)
+    {
+        answer.push_back(1);
+        
+        for (int j = 0; j < emergency.size(); ++j)
+        {
+            if (emergency[i] < emergency[j])
+            {
+                answer[i] += 1;
+            }
+        }
+    }
+    
+    
+    return answer;
+}
