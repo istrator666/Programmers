eb618f71ca571f354b71bd6a603fff966d7b2fef
Tabibito <cyh5343@naver.com>
2024-11-03 19:49:16 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    
+    vector<int> v;
+    
+    while (n)
+    {
+        v.push_back(n%3);
+        n /= 3;
+    }
+    
+    int reverse = 1;
+    
+    for (int i = 0; i < v.size(); ++i)
+    {
+        answer += v[v.size() - i - 1] * (reverse);
+        reverse *= 3;
+        
+    }
+    
+    
+    return answer;
+}
